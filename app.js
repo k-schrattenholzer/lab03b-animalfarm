67eb563e7a9fbdcc6a234564dcac6ef05660de32
‘Katie <ktschr11@gmail.com>
2021-07-15 10:58:48 -0700

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-// import functions and grab DOM elements
 
-// initialize state
+const h1El = document.getElementById('animal-name');
+const horseEl = document.getElementById('horse-image');
+const dogEl = document.getElementById('dog-image');
+const catEl = document.getElementById('cat-image');
+const horseSound = document.getElementById('horse-sound');
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+console.log(
+    h1El,
+    horseEl,
+    dogEl,
+    catEl,
+    horseSound,
+    dogSound,
+    catSound,
+);
+
+horseEl.addEventListener('click', () => {
+    horseSound.play ();
+    myh1.textContent = "Horse";
+});
+
+dogEl.addEventListener('click', () => {
+    dogSound.play ();
+    myh1.textContent = "Dog";
+});
+
+catEl.addEventListener('click', () => {
+    catSound.play ();
+    myh1.textContent = "Cat";
+});
